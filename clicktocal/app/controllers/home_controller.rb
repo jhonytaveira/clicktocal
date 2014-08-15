f0ac9917a5aa7de253b0da7aef3d5ad81d6b386b
jhonytaveira <jhony.taveira@hotmail.com>
2014-08-15 16:28:06 -0300

    Routes

--- clicktocal/app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-   class Home_Controller <  ApplicationController
-     def index
-
-     end
-   end
--- clicktocal/app/models/register_number.rb
+++ clicktocal/app/models/register_number.rb
@@ -1,8 +1,12 @@
 class RegisterNumber < ActiveRecord::Base
 
-   validates :number,presence: true
-   validates :ddd,presence: true
+  validates :number, presence: true
+  validates :ddd, presence: true
 
+
+  def arrayDDD
+    ddds = [62, 64]
+  end
 
 
 end
