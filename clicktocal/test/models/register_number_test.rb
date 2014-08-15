1a3e2ac98c0764b23cd17f67c195af2e1c0b8268
jhonytaveira <jhony.taveira@hotmail.com>
2014-08-15 13:34:29 -0300

    Teste unitário

--- clicktocal/test/models/register_number_test.rb
+++ clicktocal/test/models/register_number_test.rb
@@ -4,4 +4,18 @@ class RegisterNumberTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @register_number = register_numbers(:admin)
+  end
+
+  test "Tem que ser válidos sem alterações" do
+    assert @register_number.valid?
+  end
+
+  test "numero não pode ter nome vazio" do
+
+    assert !@register_number.valid?
+  end
+
+
 end
