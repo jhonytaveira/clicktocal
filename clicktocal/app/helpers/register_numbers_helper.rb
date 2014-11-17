7c86eb7eda93a48f384d90955360db5239911234
jhonytaveira <jhony.taveira@hotmail.com>
2014-11-17 17:13:01 -0200

    adiciona select para departamentos

--- clicktocal/app/helpers/register_numbers_helper.rb
+++ clicktocal/app/helpers/register_numbers_helper.rb
@@ -2,4 +2,17 @@ module RegisterNumbersHelper
   def get_ip
     `curl http://ipecho.net/plain`
   end
+
+  def chooses_departments(department)
+    case department
+      when  I18n.t("financial")
+        @register_number.department = 1004
+
+      when I18n.t("technical_support")
+        @register_number.department = 1008
+
+      when I18n.t("attendance")
+        @register_number.department = 1002
+    end
+  end
 end
